efeb37b569c44215755658192b8703888e38db9d
Lane Bretschneider <lanebret85@gmail.com>
2023-07-07 13:57:35 -0700

    feat: add broken count method and fix append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,16 +1,27 @@
 class LinkedList
-    attr_reader :list
+    attr_reader :head
 
     def initialize
-        @list = list
-    end
-
-    def head
-        @data
+        @head = nil
+        @current_node = nil
     end
 
     def append(data)
-        @head = Node.new(data)
+        new_node = Node.new(data)
+
+        if @head.nil?
+            @head = new_node
+        else
+            #might need loop here (*until* sort of thing)
+            @current_node = @head
+        end
+    end
+
+    def count
+        counter = 0
+        while @current_node.next_node != nil
+            counter += 1
+        end 
     end
     
 end
