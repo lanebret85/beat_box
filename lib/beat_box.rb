941df3ce20b128e4238bd8e0ed23eff680da36ca
Lane Bretschneider <lanebret85@gmail.com>
2023-07-09 15:45:43 -0700

    fix: change initialize method; feat: add require_relative

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -1,7 +1,9 @@
+require_relative "./linked_list"
+
 class BeatBox
     attr_accessor :list
 
-    def initialize
-        @list = nil
+    def initialize(list = LinkedList.new)
+        @list = list
     end
 end
