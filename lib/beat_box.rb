9bd3aaae50d332ccf192c24afca80b4f1c653d9a
Lane Bretschneider <lanebret85@gmail.com>
2023-07-10 13:09:38 -0700

    feat: add all method, needs to be refactored

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -20,6 +20,24 @@ class BeatBox
 
     def play
         beats = @list.to_string
-        `say -r 500 -v Boing #{beats}`
+        `say -r 200 -v Karen #{beats}`
     end
+
+    # refactor to make valid_beats an attribute and then filter out
+    # for not okay inputs in append method, prepend method, etc.
+    # look at "any" loop/method
+    def all
+        valid_beats = ["tee", "dee", "deep", "bop", "boop", "la", "na"]
+
+        beats = @list.to_string
+
+        valid_beats.each do |beat|
+            if beats.include?(beat) == false
+                beats.delete(beat)
+            end
+        end
+
+        beats
+    end
+
 end
